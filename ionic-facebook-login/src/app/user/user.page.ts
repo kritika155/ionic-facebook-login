8261c2a727ae537b7a48619fb9ad3d99b9820e92
kritika roy <roykritika00@gmail.com>
2019-05-09 02:30:39 +0530

    facebook-login

--- ionic-facebook-login/src/app/user/user.page.ts
+++ ionic-facebook-login/src/app/user/user.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { LoadingController } from '@ionic/angular';
+import { Facebook } from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: Facebook,
+    private nativeStorage: NativeStorage,
+    public loadingController: LoadingController,
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
+  user: any;
+  userReady: boolean = false;
+
+  async ngOnInit() {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...'
+    });
+     await loading.present();
+     this.nativeStorage.getItem('facebook_user')
+    .then(data => {
+      this.user = {
+        name: data.name,
+        email: data.email,
+        picture: data.picture
+      };
+      loading.dismiss();
+      this.userReady = true;
+    }, error =>{
+      console.log(error);
+      loading.dismiss();
+    });
+  }
+
+  doFbLogout(){
+    this.fb.logout()
+    .then(res => {
+      //user logged out so we will remove him from the NativeStorage
+      this.nativeStorage.remove('facebook_user');
+      this.router.navigate(["/login"]);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
